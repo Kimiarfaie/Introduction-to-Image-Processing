1bd6de801ee83d8b071f5551edb7bc1fb25ae2e9
Kimiarfaie <168003992+Kimiarfaie@users.noreply.github.com>
2025-10-18 18:50:47 -0700

    im2gray to rb2gray

--- Week 1-2/WeekOne.m
+++ Week 1-2/WeekOne.m
@@ -1,5 +1,5 @@
 img = imread("Image - 2230x2230.jpg");
 img1 = imresize(img,[256,256]);
 %imwrite(img1,'Image 256x256.jpg');
-img3 = im2gray(img1);
+img3 = rgb2gray(img1);
 % imwrite(img3,'Grayscale Image 256x256.jpg');
